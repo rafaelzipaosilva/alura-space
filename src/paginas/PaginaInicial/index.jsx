99bc5960c2dffb2a0e77a15d0434dbc12d4c130e
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-07-08 13:23:25 -0300

    aula2.3

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,12 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
 
 export default function PaginaInicial() {
     return (
-        <Cabecalho />
+        <>
+            <Cabecalho />
+            <Menu />
+        </>
+        
     )
 }
