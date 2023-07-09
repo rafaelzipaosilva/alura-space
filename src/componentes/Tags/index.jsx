26ae630a5f2ae29a3afc68942188ce7c25495da3
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-07-09 13:02:17 -0300

    aula5.4

--- src/componentes/Tags/index.jsx
+++ src/componentes/Tags/index.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import styles from './Tags.module.scss';
+import React from "react";
+import styles from "./Tags.module.scss";
+import fotos from "../Galeria/fotos.json";
 
-export default function Tags() {
-  return (
-    <div className={styles.tags}>
-        <p>Filtro por tags</p>
-        <ul className={styles.tags__lista}>
-            <li>Estrelas</li>
-            <li>Galáxias</li>
-            <li>Lua</li>
-            <li>Planeta</li>
-        </ul>
-    </div>
-  )
+export default function Tags({ tags, filtraFotos, setItens }) {
+	return (
+		<div className={styles.tags}>
+			<p>Filtro por tags</p>
+			<ul className={styles.tags__lista}>
+				{tags.map((tag) => {
+					return (
+						<li key={tag} onClick={() => filtraFotos(tag)}>{tag}</li>
+          );
+				})}
+        <li onClick={() => setItens(fotos)}>Todas</li>
+			</ul>
+		</div>
+	);
 }
